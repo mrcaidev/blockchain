37147d9660647126de62eef3758f8b63e1bdbdde
MrCai <1014305148@qq.com>
2021-12-06 22:38:17 +0800

    :art: 改进block包代码结构

--- core/block/pow.go
+++ core/block/pow.go
@@ -19,18 +19,18 @@ type pow struct {
 }
 
 // 创建工作量证明事件。
-func newPow(block *Block) *pow {
+func newPow(b *Block) *pow {
 	target := big.NewInt(1)
 	target.Lsh(target, uint(256-difficulty))
-	return &pow{block, target}
+	return &pow{b, target}
 }
 
-// 计算当前区块内交易的哈希值。
-func (block *Block) hashTX() []byte {
+// 计算当前区块内交易 ID 的哈希值。
+func (b *Block) hashTxID() []byte {
 	var IDs [][]byte
 
-	for _, TX := range block.Transactions {
-		IDs = append(IDs, TX.ID)
+	for _, tx := range b.Transactions {
+		IDs = append(IDs, tx.ID)
 	}
 	generalHash := sha256.Sum256(bytes.Join(IDs, []byte{}))
 
@@ -38,12 +38,12 @@ func (block *Block) hashTX() []byte {
 }
 
 // 计算当前区块的哈希值。
-func (proof *pow) hashBlock(nonce int) [32]byte {
+func (p *pow) hashBlock(nonce int) [32]byte {
 	blockBytes := bytes.Join(
 		[][]byte{
-			utils.Int64ToBytes(proof.block.Timestamp),
-			proof.block.hashTX(),
-			proof.block.PrevBlockHash,
+			utils.Int64ToBytes(p.block.Timestamp),
+			p.block.hashTxID(),
+			p.block.PrevBlockHash,
 			utils.Int64ToBytes(int64(nonce)),
 			utils.Int64ToBytes(int64(difficulty)),
 		},
@@ -54,19 +54,18 @@ func (proof *pow) hashBlock(nonce int) [32]byte {
 }
 
 // 供自身判断工作量是否被证明。
-func (proof *pow) isProved(hash [32]byte) bool {
+func (p *pow) isProved(hash [32]byte) bool {
 	hashInt := utils.BytesToBigInt(hash[:])
-	return hashInt.Cmp(proof.target) == -1
+	return hashInt.Cmp(p.target) == -1
 }
 
 // 开始证明工作量。
-func (proof *pow) Run() {
+func (p *pow) Run() {
 	for nonce := 0; nonce < maxNonce; {
-		hash := proof.hashBlock(nonce)
-
-		if proof.isProved(hash) {
-			proof.block.Hash = hash[:]
-			proof.block.Nonce = nonce
+		hash := p.hashBlock(nonce)
+		if p.isProved(hash) {
+			p.block.Hash = hash[:]
+			p.block.Nonce = nonce
 			break
 		}
 		nonce++
